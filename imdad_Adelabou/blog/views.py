5cbe2bc612a404866f49d5c19ccffdc5855ed1f8
imdadadelabou0 <imdadadelabou0@gmail.com>
2022-06-25 18:35:19 +0100

    [feat] create a PostCreateView

--- imdad_Adelabou/blog/views.py
+++ imdad_Adelabou/blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic.list import ListView
+from django.views.generic.edit.CreateView import CreateView
 from .models import Post
 
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
